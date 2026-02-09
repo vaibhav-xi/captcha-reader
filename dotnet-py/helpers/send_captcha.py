2699df8e8a8ec1f9070268082595198f5bba9b73
vaibhav-xi <vaibhav28890@gmail.com>
2026-02-09 20:59:05 +0530

    Save Payload to txt file when sending data to API

--- dotnet-py/helpers/send_captcha.py
+++ dotnet-py/helpers/send_captcha.py
@@ -2,6 +2,7 @@ import base64
 import requests
 import sys
 import os
+import json
 
 if len(sys.argv) != 2:
     print("Usage: python send_captcha.py image.png")
@@ -12,12 +13,20 @@ path = sys.argv[1]
 with open(path, "rb") as f:
     b64 = base64.b64encode(f.read()).decode()
 
+image_name = os.path.basename(path)
+name_no_ext = os.path.splitext(image_name)[0]
+
 payload = {
     "base64": b64,
-    "fileName": os.path.basename(path)
+    "fileName": image_name
 }
 
-print("base64 String: ", b64)
+with open(f"{name_no_ext}.txt", "w") as out:
+    json.dump(payload, out, indent=2)
+
+print("Saved payload to:", f"{name_no_ext}.txt")
+
+print("base64 String:", b64)
 
 r = requests.post(
     "http://127.0.0.1:8000/predict_base64",
