79760fce06455f6e31cf62d3d21466a4d125104f
vaibhav-xi <vaibhav28890@gmail.com>
2026-02-11 05:45:30 +0530

    organise stuff

--- orange_captcha/keras-h5.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-print("Loading keras v3 model...")
-m = tf.keras.models.load_model("ocr_ctc_infer.keras", compile=False)
-
-print("Saving legacy H5 model...")
-m.save("old_model_legacy.h5")
-
-print("Done → old_model_legacy.h5")
